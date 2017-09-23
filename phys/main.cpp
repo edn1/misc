970d520b99ebbf540c94050dc72d907e124e4adb
Erez D <erez0001@gmail.com>
2017-09-23 13:22:10 +0300

    fixes

--- phys/main.cpp
+++ phys/main.cpp
@@ -6,10 +6,19 @@
 int main()
 {
   try {
-    Matrix m({{1,2,3,4},{5,6,7,8},{9,10,11,12}});
-    std::cout << m;
-  } catch ( const char * err) {
-    std::cout << "Error: " << err << '\n';
+    //Matrix m({{1,2,3,4},{5,6,7,8},{9,10,11,12}});
+    //std::cout << m;
+    auto v1=Vector({1,2,3});
+    auto v2=Vector({2,3,4});
+    auto v3=v1.dot(v2);
+    auto v4=v1+v2;
+    std::cout << "v1= " << v1;
+    std::cout << "v2= " << v2;
+    std::cout << "v1 dot v2 = " << v3;
+    std::cout << "v1 + v2 = " << v4;
+    return 0;
+  } catch ( ... ) {
+    return 1;
   }
 }
 
--- phys/matrix.h
+++ phys/matrix.h
@@ -2,23 +2,29 @@
 
 #include <vector>
 #include <sstream>
+#include <iostream>
 typedef unsigned int uint;
 
+void Throw(const std::string &msg) {
+  std::cout << "Error: " << msg;
+  throw "";
+}
 class Matrix
 {
   uint m_width;
   uint m_height;
   std::vector<std::vector<double> > m_mem;
   public:
-  Matrix(uint width,uint height):m_width(width),m_height(height),m_mem(height,std::vector<double>(height,0.0)) {}
+  Matrix(uint height,uint width):m_width(width),m_height(height),m_mem(height,std::vector<double>(height,0.0)) {}
   Matrix(const Matrix &other):m_width(other.m_width),m_height(other.m_height),m_mem(other.m_mem) {};
-  Matrix(const std::vector<std::vector<double>> &v):m_width(v[0].size()),m_height(v.size()),m_mem(v) {for (uint i=0;i<m_height;i++) if (m_mem[i].size()!=m_width) throw "DimensionsMismatch";}
+  Matrix(const std::vector<std::vector<double>> &v):m_width(v.size() ? v[0].size():0),m_height(v.size()),m_mem(v) {for (uint i=0;i<m_height;i++) if (m_mem[i].size()!=m_width) Throw("DimensionsMismatch");}
   Matrix operator*(const Matrix &rhs) const
   {
     if(m_width!=rhs.m_height)
-      throw "DimensionsMismatch";
-    Matrix ret(m_height,rhs.m_height);
+      Throw("DimensionsMismatch");
+    Matrix ret(m_height,rhs.m_width);
     for (uint i=0;i<ret.m_height;i++)
+    {
       for (uint j=0;j<ret.m_width;j++)
       {
         double v=0;
@@ -26,33 +32,35 @@ class Matrix
           v+=m_mem[i][k]*rhs.m_mem[k][j];
         ret.m_mem[i][j]=v;
       }
+    }
     return ret;
   }
   Matrix operator+(const Matrix &rhs) const
   {
     if (m_width!=rhs.m_width  ||
         m_height !=rhs.m_height)
-      throw "DimensionsMismatch";
-    Matrix ret(m_width,m_height);
+      Throw("DimensionsMismatch");
+    Matrix ret(m_height,m_width);
     for (uint i=0;i<ret.m_height;i++)
       for (uint j=0;j<ret.m_width;j++)
         ret.m_mem[i][j]=m_mem[i][j]+rhs.m_mem[i][j];
+    return ret;
   }
   const double &at(uint i,uint j) const
   {
     if (i>m_height || j>m_width)
-      throw "DimensionsMismatch";
+      Throw("DimensionsMismatch");
     return m_mem[i][j];
   }
   double &at(uint i,uint j)
   {
     if (i>m_height || j>m_width)
-      throw "DimensionsMismatch";
+      Throw("DimensionsMismatch");
     return m_mem[i][j];
   }
   Matrix operator+(const double &rhs) const
   {
-    Matrix ret(m_width,m_height);
+    Matrix ret(m_height,m_width);
     for (uint i=0;i<ret.m_height;i++)
       for (uint j=0;j<ret.m_width;j++)
         ret.m_mem[i][j]=m_mem[i][j]+rhs;
@@ -75,8 +83,26 @@ class Matrix
     }
     return ret.str();
   }
+  Matrix t() const // transpose
+  {
+    Matrix ret(m_width,m_height);
+    for (uint i=0;i<ret.m_height;i++)
+      for (uint j=0;j<ret.m_width;j++)
+        ret.m_mem[i][j]=m_mem[j][i];
+    return ret;
+  }
+  Matrix dot(const Matrix &rhs) const 
+  {
+    if ( ( m_height!=1 && m_width!=1 ) || m_height!=rhs.m_height || m_width!=rhs.m_width)
+      Throw("DimensionsMismatch");
+    if (m_height==1)
+      return ((*this)*(rhs.t()));
+    else
+      return (t()*rhs);
+  }
 };
 
+Matrix Vector(const std::vector<double> &v) {return Matrix(std::vector<std::vector<double>>(1,v));}
 std::ostream &operator<<(std::ostream &os, Matrix const &m) { 
     return os << m.toString();
 }
